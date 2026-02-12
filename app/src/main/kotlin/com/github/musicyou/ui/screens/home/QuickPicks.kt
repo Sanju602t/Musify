00aa2dc3f2b3e9fea578f8017d5d90ec383c7af2
Sanju602t <ddaon97@gmail.com>
2026-02-12 22:29:26 +0530

    Update QuickPicks.kt

--- app/src/main/kotlin/com/github/musicyou/ui/screens/home/QuickPicks.kt
+++ app/src/main/kotlin/com/github/musicyou/ui/screens/home/QuickPicks.kt
@@ -1,53 +1,21 @@
 package com.github.musicyou.ui.screens.home
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.item
-import androidx.compose.foundation.lazy.items
+
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.DownloadForOffline
-import androidx.compose.material.icons.outlined.Refresh
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.times
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.innertube.Innertube
 import com.github.innertube.models.NavigationEndpoint
@@ -59,25 +27,14 @@ import com.github.musicyou.enums.QuickPicksSource
 import com.github.musicyou.models.LocalMenuState
 import com.github.musicyou.query
 import com.github.musicyou.ui.components.HomeScaffold
-import com.github.musicyou.ui.components.ShimmerHost
 import com.github.musicyou.ui.components.NonQueuedMediaItemMenu
-import com.github.musicyou.ui.components.TextPlaceholder
-import com.github.musicyou.ui.items.AlbumItem
-import com.github.musicyou.ui.items.ArtistItem
-import com.github.musicyou.ui.items.ItemPlaceholder
-import com.github.musicyou.ui.items.ListItemPlaceholder
 import com.github.musicyou.ui.items.LocalSongItem
-import com.github.musicyou.ui.items.PlaylistItem
 import com.github.musicyou.ui.items.SongItem
-import com.github.musicyou.ui.styling.Dimensions
-import com.github.musicyou.utils.SnapLayoutInfoProvider
 import com.github.musicyou.utils.asMediaItem
 import com.github.musicyou.utils.forcePlay
-import com.github.musicyou.utils.isLandscape
 import com.github.musicyou.utils.quickPicksSourceKey
 import com.github.musicyou.utils.rememberPreference
 import com.github.musicyou.viewmodels.QuickPicksViewModel
-import kotlinx.coroutines.launch
 
 @ExperimentalFoundationApi
 @ExperimentalAnimationApi
@@ -122,14 +79,11 @@ fun QuickPicks(
                     Text(
                         text = stringResource(id = R.string.quick_picks),
                         style = MaterialTheme.typography.titleMedium,
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp)
-                            .padding(bottom = 8.dp)
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
                     )
                 }
 
                 item {
-
                     LazyRow(
                         contentPadding = PaddingValues(horizontal = 16.dp),
                         horizontalArrangement = Arrangement.spacedBy(12.dp)
