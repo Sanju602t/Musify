da33b214d1bcb3249afd20ceb93ddcc6aa174e8e
Sanju602t <ddaon97@gmail.com>
2026-02-12 21:51:17 +0530

    Refactor QuickPicks layout and remove unused code

--- app/src/main/kotlin/com/github/musicyou/ui/screens/home/QuickPicks.kt
+++ app/src/main/kotlin/com/github/musicyou/ui/screens/home/QuickPicks.kt
@@ -93,15 +93,10 @@ fun QuickPicks(
     val playerPadding = LocalPlayerPadding.current
 
     val viewModel: QuickPicksViewModel = viewModel()
-    val quickPicksSource by rememberPreference(quickPicksSourceKey, QuickPicksSource.Trending)
-    val scope = rememberCoroutineScope()
-
-    val songThumbnailSizeDp = Dimensions.thumbnails.song
-    val itemSize = 108.dp + 2 * 8.dp
-    val quickPicksLazyGridState = rememberLazyGridState()
-    val sectionTextModifier = Modifier
-        .padding(horizontal = 16.dp)
-        .padding(bottom = 8.dp)
+    val quickPicksSource by rememberPreference(
+        quickPicksSourceKey,
+        QuickPicksSource.Trending
+    )
 
     LaunchedEffect(quickPicksSource) {
         viewModel.loadQuickPicks(quickPicksSource = quickPicksSource)
@@ -112,59 +107,45 @@ fun QuickPicks(
         openSearch = openSearch,
         openSettings = openSettings
     ) {
-        BoxWithConstraints {
-            val quickPicksLazyGridItemWidthFactor =
-                if (isLandscape && maxWidth * 0.475f >= 320.dp) 0.475f else 0.9f
 
-            val density = LocalDensity.current
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 16.dp + playerPadding)
+        ) {
 
-            val snapLayoutInfoProvider = remember(quickPicksLazyGridState) {
-                with(density) {
-                    SnapLayoutInfoProvider(
-                        lazyGridState = quickPicksLazyGridState,
-                        positionInLayout = { layoutSize, itemSize ->
-                            (layoutSize * quickPicksLazyGridItemWidthFactor / 2f - itemSize / 2f)
-                        }
-                    )
-                }
-            }
+            viewModel.relatedPageResult?.getOrNull()?.let { related ->
 
-            val itemInHorizontalGridWidth = maxWidth * quickPicksLazyGridItemWidthFactor
-
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-                    .padding(top = 4.dp, bottom = 16.dp + playerPadding)
-            ) {
-                viewModel.relatedPageResult?.getOrNull()?.let { related ->
+                item {
                     Text(
                         text = stringResource(id = R.string.quick_picks),
                         style = MaterialTheme.typography.titleMedium,
-                        modifier = sectionTextModifier
-                    )
-
-                    LazyHorizontalGrid(
-                        state = quickPicksLazyGridState,
-                        rows = GridCells.Fixed(count = 4),
-                        flingBehavior = rememberSnapFlingBehavior(snapLayoutInfoProvider),
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .height((songThumbnailSizeDp + Dimensions.itemsVerticalPadding * 2) * 4)
+                            .padding(horizontal = 16.dp)
+                            .padding(bottom = 8.dp)
+                    )
+                }
+
+                item {
+
+                    LazyRow(
+                        contentPadding = PaddingValues(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
                     ) {
+
                         viewModel.trending?.let { song ->
                             item {
                                 LocalSongItem(
-                                    modifier = Modifier
-                                        .animateItem()
-                                        .width(itemInHorizontalGridWidth),
+                                    modifier = Modifier.width(240.dp),
                                     song = song,
                                     onClick = {
                                         val mediaItem = song.asMediaItem
                                         binder?.stopRadio()
                                         binder?.player?.forcePlay(mediaItem)
                                         binder?.setupRadio(
-                                            NavigationEndpoint.Endpoint.Watch(videoId = mediaItem.mediaId)
+                                            NavigationEndpoint.Endpoint.Watch(
+                                                videoId = mediaItem.mediaId
+                                            )
                                         )
                                     },
                                     onLongClick = {
@@ -187,21 +168,21 @@ fun QuickPicks(
                         }
 
                         items(
-                            items = related.songs?.dropLast(if (viewModel.trending == null) 0 else 1)
-                                ?: emptyList(),
+                            items = related.songs ?: emptyList(),
                             key = Innertube.SongItem::key
                         ) { song ->
+
                             SongItem(
-                                modifier = Modifier
-                                    .animateItem()
-                                    .width(itemInHorizontalGridWidth),
+                                modifier = Modifier.width(240.dp),
                                 song = song,
                                 onClick = {
                                     val mediaItem = song.asMediaItem
                                     binder?.stopRadio()
                                     binder?.player?.forcePlay(mediaItem)
                                     binder?.setupRadio(
-                                        NavigationEndpoint.Endpoint.Watch(videoId = mediaItem.mediaId)
+                                        NavigationEndpoint.Endpoint.Watch(
+                                            videoId = mediaItem.mediaId
+                                        )
                                     )
                                 },
                                 onLongClick = {
@@ -215,173 +196,6 @@ fun QuickPicks(
                                     }
                                 }
                             )
-                        }
-                    }
-
-                    related.albums?.let { albums ->
-                        Spacer(modifier = Modifier.height(Dimensions.spacer))
-
-                        Text(
-                            text = stringResource(id = R.string.related_albums),
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = sectionTextModifier
-                        )
-
-                        LazyRow(
-                            contentPadding = PaddingValues(horizontal = 8.dp)
-                        ) {
-                            items(
-                                items = albums,
-                                key = Innertube.AlbumItem::key
-                            ) { album ->
-                                AlbumItem(
-                                    modifier = Modifier.widthIn(max = itemSize),
-                                    album = album,
-                                    onClick = { onAlbumClick(album.key) }
-                                )
-                            }
-                        }
-                    }
-
-                    related.artists?.let { artists ->
-                        Spacer(modifier = Modifier.height(Dimensions.spacer))
-
-                        Text(
-                            text = stringResource(id = R.string.similar_artists),
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = sectionTextModifier
-                        )
-
-                        LazyRow(
-                            contentPadding = PaddingValues(horizontal = 8.dp)
-                        ) {
-                            items(
-                                items = artists,
-                                key = Innertube.ArtistItem::key,
-                            ) { artist ->
-                                ArtistItem(
-                                    modifier = Modifier.widthIn(max = itemSize),
-                                    artist = artist,
-                                    onClick = { onArtistClick(artist.key) }
-                                )
-                            }
-                        }
-                    }
-
-                    related.playlists?.let { playlists ->
-                        Spacer(modifier = Modifier.height(Dimensions.spacer))
-
-                        Text(
-                            text = stringResource(id = R.string.recommended_playlists),
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = sectionTextModifier
-                        )
-
-                        LazyRow(
-                            contentPadding = PaddingValues(horizontal = 8.dp)
-                        ) {
-                            items(
-                                items = playlists,
-                                key = Innertube.PlaylistItem::key,
-                            ) { playlist ->
-                                PlaylistItem(
-                                    modifier = Modifier.widthIn(max = itemSize),
-                                    playlist = playlist,
-                                    onClick = { onPlaylistClick(playlist.key) }
-                                )
-                            }
-                        }
-                    }
-
-                    Unit
-                } ?: viewModel.relatedPageResult?.exceptionOrNull()?.let {
-                    Text(
-                        text = stringResource(id = R.string.home_error),
-                        style = MaterialTheme.typography.titleMedium,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .padding(all = 16.dp)
-                    )
-
-                    Row(
-                        modifier = Modifier.align(Alignment.CenterHorizontally),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Button(
-                            onClick = {
-                                scope.launch {
-                                    viewModel.loadQuickPicks(quickPicksSource)
-                                }
-                            }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Outlined.Refresh,
-                                contentDescription = stringResource(id = R.string.retry)
-                            )
-
-                            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-
-                            Text(text = stringResource(id = R.string.retry))
-                        }
-
-                        FilledTonalButton(
-                            onClick = onOfflinePlaylistClick
-                        ) {
-                            Icon(
-                                imageVector = Icons.Outlined.DownloadForOffline,
-                                contentDescription = stringResource(id = R.string.offline)
-                            )
-
-                            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-
-                            Text(text = stringResource(id = R.string.offline))
-                        }
-                    }
-                } ?: ShimmerHost {
-                    TextPlaceholder(modifier = sectionTextModifier)
-
-                    repeat(4) {
-                        ListItemPlaceholder()
-                    }
-
-                    Spacer(modifier = Modifier.height(Dimensions.spacer))
-
-                    TextPlaceholder(modifier = sectionTextModifier)
-
-                    Row(
-                        modifier = Modifier.padding(start = 8.dp)
-                    ) {
-                        repeat(2) {
-                            ItemPlaceholder(modifier = Modifier.widthIn(max = itemSize))
-                        }
-                    }
-
-                    Spacer(modifier = Modifier.height(Dimensions.spacer))
-
-                    TextPlaceholder(modifier = sectionTextModifier)
-
-                    Row(
-                        modifier = Modifier.padding(start = 8.dp)
-                    ) {
-                        repeat(2) {
-                            ItemPlaceholder(
-                                modifier = Modifier.widthIn(max = itemSize),
-                                shape = CircleShape
-                            )
-                        }
-                    }
-
-                    Spacer(modifier = Modifier.height(Dimensions.spacer))
-
-                    TextPlaceholder(modifier = sectionTextModifier)
-
-                    Row(
-                        modifier = Modifier.padding(start = 8.dp)
-                    ) {
-                        repeat(2) {
-                            ItemPlaceholder(modifier = Modifier.widthIn(max = itemSize))
                         }
                     }
                 }
